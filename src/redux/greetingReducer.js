942f9556f593d131ce8a0d483ebbce1fe2d94a43
J. Luis Arredondo <jose.arredondo.a@outlook.com>
2022-08-10 01:55:56 -0500

    Add 'greeting' reducer

---
+++ src/redux/greetingReducer.js
@@ -0,0 +1,26 @@
+const baseLink = 'http://localhost:3000/api/greetings.json';
+
+const FETCH_GREETINGS = 'FETCH_GREETINGS';
+const initialState = { message: '...' };
+
+const getGreetings = async () => {
+  const response = await fetch(baseLink);
+  const data = await response.json();
+  return data;
+};
+
+export const fetchGreetings = () => async dispatch => {
+  const data = await getGreetings();
+  dispatch({ type: FETCH_GREETINGS, payload: data });
+};
+
+const greetingReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case FETCH_GREETINGS:
+      return action.payload;
+    default:
+      return state;
+  }
+};
+
+export default greetingReducer;
